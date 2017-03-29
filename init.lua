e28dc228f0109b887d822144135dda60e85afc8b
iotserv <yu@iotserv.com>
2017-03-29 14:00:54 +0800

    Update init.lua
    fix bug

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 dofile('cmd.lua')
-id="456" --you can set your id by youself!!!!!!
+id="4567" --you can set your id by youself!!!!!!
 function startServer()
 print(wifi.sta.getip())
 sk=net.createConnection(net.TCP, 0)
@@ -10,20 +10,23 @@ sk:on("connection", function(sck, c)
 print(c)
 sk:send(id)
 function s_output(str)
-         if (sk~=nil)    then
+         if (sk~=nil and str~='')    then
             sk:send(str)
          end
       end
 node.output(s_output,1)
 end )
-sk:on("disconnection",function(conn) 
-         node.output(nil) 
+sk:on("disconnection",function(conn,c) 
+         --node.output(nil) 
+		 print('reconnect')
+		 sk:connect(8001,"www.mcunode.com")
+		 sk:send(id)
       end)
-sk:connect(80,"www.mcunode.com")
+sk:connect(8001,"www.mcunode.com")
 end
 wifi.setmode(wifi.STATION)
-wifi.sta.config("ssid","password")    --set your ap info !!!!!!
-wifi.sta.connect()
+wifi.sta.config("wifi","123456abc")    --set your ap info !!!!!!
+wifi.sta.autoconnect(1)
 tmr.alarm(1, 1000, 1, function() 
    if wifi.sta.getip()==nil then
       print("Connect AP, Waiting...") 
