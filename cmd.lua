652c3a9f33cab824eac7e26d4492c116dfdb2c80
iotserv <yu@iotserv.com>
2017-05-15 17:43:13 +0800

    Update cmd.lua

--- cmd.lua
+++ cmd.lua
@@ -1,3 +1,4 @@
+--new Version NOT NEED
 function ls()
 	local l = file.list()
 	for k,v in pairs(l) do
@@ -17,33 +18,4 @@ function cat(filename)
 		print(line)
 	end
 	file.close()
-end
-
-function getFile(p,f)
-  local strPost = "GET /"..p.."f="..f.." HTTP/1.0\r\nHost: "..'www.mcunode.com'.."\r\n\r\n"
-  local sk=net.createConnection(net.TCP, 0)
-
-  local s=0
-  file.remove(f)
-  file.open(f,"a")
-
-  sk:on("connection", function(sck) sk:send(strPost) end)
-
-  sk:on("receive", function(sck, res)
-     local pos=string.find(res,"%c%c%c")
-     if pos~=nil then
-          s=1
-          pos=pos+4
-     else
-        pos=0
-     end
-     if s==1 then
-          file.write(res:sub(pos))
-     end
-     res=nil 
-     collectgarbage() 
-  end)
-
-  sk:dns(host,function(conn,ip) sk:connect(80,ip) end)
-  tmr.alarm(5,2000,0,function() sk:close() sk=nil strPost = nil file.close() collectgarbage() end)
 end
